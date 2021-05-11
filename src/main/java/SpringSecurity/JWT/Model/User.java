1f015e5474e609f94003e370579a564549984cfe
Amine-BELLA <aminebella103112@gmail.com>
2021-05-11 12:59:45 +0000

    Spring Security config

---
+++ src/main/java/SpringSecurity/JWT/Model/User.java
@@ -0,0 +1,22 @@
+package SpringSecurity.JWT.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+}
---
+++ src/main/java/SpringSecurity/JWT/Service/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package SpringSecurity.JWT.Service;
+
+import SpringSecurity.JWT.Model.User;
+import SpringSecurity.JWT.Repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>());
+    }
+}
